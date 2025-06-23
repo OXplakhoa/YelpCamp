4d20550cc77e01b6b940a01ace445802d98466ff
OXplakhoa <accbangbangchuot@gmail.com>
2025-06-23 09:36:06 +0700

    Add API

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const reviewsRoutes = require('./routes/reviews.js')
 
 const MongoDBStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl);
 
@@ -44,7 +44,7 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')))
 app.use(mongoSanitize());
 
-const secret = 'thisisasecrethehe';
+const secret = process.env.SECRET || 'thisisasecrethehe';
 
 const store = MongoDBStore.create({
     mongoUrl: dbUrl,
@@ -157,7 +157,7 @@ app.use((err,req,res,next) => {
     res.status(statusCode).render('error',{err})
 })
 
-
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
